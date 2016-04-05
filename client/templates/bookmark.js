ad42890860aa5920d33f8de85ac39dbede9ff9d6
coproductions <constantin.dumba@gmail.com>
2016-04-05 13:08:09 -0700

    modal button

--- client/templates/bookmark.js
+++ client/templates/bookmark.js
@@ -1,5 +1,10 @@
-Template.bookmark.helpers({
+Template.bookmark.events({
   // rating: function(){
 
   // }
+
+  'click .showModal' : function(evt, tmplt){
+    console.log('show mod');
+    $('#bookmark-modal').modal('show');
+  }
 })
